9ca8f74b9d95b688b504434034f896bbd0480281
Phillip MacEachron <phillipmaceachron@Phillips-MacBook-Pro.local>
2019-11-15 08:26:13 -0800

    api call working, and displaying image for each character

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
-import Header from "./components/Header.js";
+import {Route, NavLink} from 'react-router-dom';
 
+import Navigation from "./components/Navigation.js";
+import Header from "./components/Header.js";
+import CharacterList from './components/CharacterList';
 
 export default function App() {
   return (
     <main>
+      <Navigation />
       <Header />
+      <CharacterList />
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,27 @@
 import React, { useEffect, useState } from "react";
-
+import {Grid} from '@material-ui/core';
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [characterList, setCharacterList] = useState([]);
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    fetch('https://rickandmortyapi.com/api/character/')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.results);
+      setCharacterList(data.results);
+    })
   }, []);
 
   return (
     <section className="character-list">
       <h2>TODO: `array.map()` over your state here!</h2>
+      <Grid container>
+        {characterList.map((character, index) => (
+          <Grid item xs={4} key={index}>
+            <img src={character.image} style={{width:'100%'}}/>
+          </Grid>
+        ))}
+      </Grid>
     </section>
   );
 }
